0cbc0c0a67cda28e52f92f625003273f3d330bf8
Pumpkinmann <96442304+Pumpkinmann@users.noreply.github.com>
2023-05-23 15:33:49 +0800

    Add files via upload

---
+++ Dice_Game/index.js
@@ -0,0 +1,16 @@
+randomNumber1 = Math.floor(Math.random() * 6 + 1);
+randomNumber2 = Math.floor(Math.random() * 6 + 1);
+result = document.querySelectorAll("div > img") ;
+result[0].src = `images/dice${randomNumber1}.png` ;
+result[1].src = `images/dice${randomNumber2}.png` ;
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 Wins!"
+}
+
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").textContent = "Player 2 Wins!"
+}
+
+else {
+    document.querySelector("h1").textContent = "Draw!"
+}
