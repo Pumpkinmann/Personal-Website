86f250fa2c531b60b20d469ce1ac8f24dd35d832
Pumpkinmann <96442304+Pumpkinmann@users.noreply.github.com>
2023-05-23 15:38:35 +0800

    Update index.js

--- Dice_Game/index.js
+++ Dice_Game/index.js
@@ -1,8 +1,8 @@
 randomNumber1 = Math.floor(Math.random() * 6 + 1);
 randomNumber2 = Math.floor(Math.random() * 6 + 1);
 result = document.querySelectorAll("div > img") ;
-result[0].src = `images/dice${randomNumber1}.png` ;
-result[1].src = `images/dice${randomNumber2}.png` ;
+result[0].src = `Images/dice${randomNumber1}.png` ;
+result[1].src = `Images/dice${randomNumber2}.png` ;
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").textContent = "Player 1 Wins!"
 }
